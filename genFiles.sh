0a85cd61230ca48e9c0b91eee93e39c869f4c162
Seth McNeill <seth@anitracks.com>
2014-12-29 06:26:56 -0600

    genFiles now downloads the needed files before running generateDS.py

--- genFiles.sh
+++ genFiles.sh
@@ -5,6 +5,12 @@
 # Seth McNeill
 # 2013 October 18
 
+# requires that the generateDS.py package be installed first.
+
+# collect the needed files from the internet
+wget "http://graphical.weather.gov/xml/DWMLgen/schema/DWML.xsd"
+wget "http://www.nws.noaa.gov/view/current_observation.xsd"
+
 generateDS.py -f \
   -o ForeWx_api.py \
   -s ForeWx_proc.py \
